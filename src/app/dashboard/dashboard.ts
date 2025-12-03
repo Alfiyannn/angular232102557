430ff6b95269ec35606d20aaeb495eb70ee785f3
Alfiyannn <alfiyanblogger@gmail.com>
2025-12-03 19:15:30 +0700

    sh

--- src/app/dashboard/dashboard.ts
+++ src/app/dashboard/dashboard.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { AfterViewInit, Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 import { Header } from "../header/header";
 import { Sidebar } from "../sidebar/sidebar";
 import { Footer } from "../footer/footer";
+
+declare const $: any;
 
 @Component({
   selector: 'app-dashboard',
   imports: [RouterModule, Header, Sidebar, Footer],
   templateUrl: './dashboard.html',
-  styleUrl: './dashboard.css',
+  styleUrls: ['./dashboard.css'],
 })
-export class Dashboard {
+export class Dashboard implements AfterViewInit {
 
+  constructor(private router: Router) { }
+
+  ngAfterViewInit() {
+    this.router.events.subscribe(() => {
+      $('body')
+        .addClass('sidebar-closed sidebar-collapse')
+        .removeClass('sidebar-open');
+    });
+  }
 }
